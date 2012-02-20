cb7bd7548540ad102fb59e27f4b3c56241820756
Martijn The <post@martijnthe.nl>
2012-02-20 08:07:29 +0100

    'rows' are already dealloc'd when the pointer is returned, because 'callback' is dealloc'd

--- Source/MySQL/CSMySQLDatabase.m
+++ Source/MySQL/CSMySQLDatabase.m
@@ -211,7 +211,7 @@
                            receiver:callback
                             selector:@selector(rowsAsDictionaries:)
                               error:error];
-    NSMutableArray *rows = [callback rows];
+    NSMutableArray *rows = [[[callback rows] retain] autorelease];
 	[callback release];
     return success ? rows : nil;
 }
@@ -226,7 +226,7 @@
                            receiver:callback
 						   selector:@selector(rowsAsArrays:) 
                               error:error];
-    NSMutableArray *rows = [callback rows]; 
+    NSMutableArray *rows = [[[callback rows] retain] autorelease];
 	[callback release];
     return success ? rows : nil;
 }
